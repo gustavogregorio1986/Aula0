997fab28403a09b7ce2ae1a89e68395cac0faef7
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2025-12-14 20:49:35 -0300

    Desenvolvendo a logica de programação

---
+++ Aula07/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Aula07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("========Operadores de Atribuição===========");
+
+            // Inicializar a variável
+            int x = 10;
+            Console.WriteLine("\nValor inicial da Veriavel de x: " + x);
+
+            //Opertador de Atribuição (=)
+            x = 20;
+            Console.WriteLine("Atribuição Simples");
+            Console.WriteLine("Valor de x: " + x);
+
+
+            //Atribuição Composta
+            x++; // x = x + 20
+
+            Console.WriteLine("\nAtribuição Composta Incremento (++)");
+            Console.WriteLine("\nNovo Valor de x: (++) " + x);
+
+            x += 10;
+            Console.WriteLine("\nAtribuiçãao Composta (+=)");
+            Console.WriteLine("Novo Valor de x: " + x);
+
+            x *= 10;
+            Console.WriteLine("\nAtribuiçãao Composta (*=)");
+            Console.WriteLine("Novo Valor de x: " + x);
+
+            x /= 10;
+            Console.WriteLine("\nAtribuiçãao Composta (/=)");
+            Console.WriteLine("Novo Valor de x: " + x);
+
+        }
+    }
+   
+}
---
+++ Aula08/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Aula07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=============Operadores Relçacionais==============");
+            Console.WriteLine("Digite o primeiro numwero: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o segundo numero: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            string result;
+
+            if(x == y)
+            {
+                result = "x é igual a y";
+            }
+            else
+            {
+                result = "x é diferente de y";
+            }
+
+            Console.WriteLine("\nIgualdade (==): " + result);
+
+            //Operador de Diferença (!=)
+            if (x != y)
+            {
+                result = "x é diferente a y";
+            }
+            else
+            {
+                result = "x é igual de y";
+            }
+
+            Console.WriteLine("\nDiferença (!=): " + result);
+
+            if (x < y)
+            {
+                result = "x é menor que y";
+            }
+            else
+            {
+                result = "x é maior que y";
+            }
+
+            Console.WriteLine("\nMenor que (<): " + result);
+
+
+            if (x <= y)
+            {
+                result = "x é menor ou igual y";
+            }
+            else
+            {
+                result = "x é maior ou igual  y";
+            }
+
+            Console.WriteLine("\nMenor ou igual (<): " + result);
+        }
+    }
+}
