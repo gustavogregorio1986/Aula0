efe5df9631c2b08d319271ea6c4ce690586d8590
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2025-12-14 20:57:35 -0300

    Desenvolvendo porjetos incriveis com C# basico

---
+++ Aula09/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Aula07
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("============Operadores Lógicos==================");
+            
+            bool isLogged = true;
+            bool hasAdminAccess = false;
+
+            Console.WriteLine("\nInformações do Usuário");
+            Console.WriteLine("Usuário logado:");
+            Console.WriteLine("Acesso ao painel de administrador: " + hasAdminAccess);
+
+            //Operador (||) - OU
+            if (isLogged || hasAdminAccess)
+            {
+                Console.WriteLine("Acesso concedido ao sistema.");
+            }
+            
+
+            //operador (&&) - E
+            if (isLogged && hasAdminAccess)
+            {
+                Console.WriteLine("Acesso concedido ao painel de administração.");
+            }
+            else
+            {
+                Console.WriteLine("Acesso negado ao painel de administração.");
+            }
+        }
+    }
+}
