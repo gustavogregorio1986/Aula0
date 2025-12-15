4b0cee91850015fe7fb57ff928c68b2f93a78787
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2025-12-14 21:58:24 -0300

    Desenvolver para estudar logica com muiota dedicação e aprendizado

---
+++ Aula12/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Aula12
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //int i = 0;
+
+            //while (i <= 1000)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+
+            //Ex2:
+            double budget = 100;
+            while(budget > 0)
+            {
+                Console.WriteLine(budget);
+                budget--;
+            }
+        }
+    }
+}
---
+++ Aula14/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Aula14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+
+               for(condicao 1; condicao 2; condicao 3)
+               {
+                     //bloco de codigo
+               }
+
+             Declaração 1 é executada uma vez no início do loop.
+               
+             Declaração 2 é a condição de continuação do loop. Enquanto   for verdadeira, o loop continua.
+             
+             Declaração 3 é executada no final de cada iteração do loop.
+            */
+
+            //ex1:
+            for(int i = 1; i <= 10; i++)
+            {
+                for(int j = 1; j <= 10; j++)
+                {
+                    Console.Write($" {i * j, 6} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
